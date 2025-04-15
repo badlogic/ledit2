5c44f7e40c8c682f041b062845bc2e764ce0fdc3
Mario Zechner <badlogicgames@gmail.com>
2025-04-15 22:51:03 +0200

    Fetch reddit posts client-side

--- src/apis/reddit.ts
+++ src/apis/reddit.ts
@@ -123,9 +123,11 @@ export class RedditStream extends Stream<RedditPost> {
                     sortParam +
                     "&" +
                     (cursor ? "after=" + cursor : "");
-                const response = await Api.proxyJson<RedditPosts>(url);
-                if (response instanceof Error) throw response;
-                const page = response;
+
+                const response = await fetch(url);
+                if (!response.ok) throw new Error(await response.text());
+                const page = await response.json() as RedditPosts;
+
                 if (!page || !page.data || !page.data.children) throw new Error("No data in response");
                 const posts: RedditPost[] = [];
                 for (const post of page.data.children) {
